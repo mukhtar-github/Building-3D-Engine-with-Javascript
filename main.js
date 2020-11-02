43342a5da9e099d66adbdd7b489c1a0519a60444
Mukhtar <mukhtartanimugarba@gmail.com>
2020-11-02 21:19:01 +0100

    File update

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-// import {Vertex} from './vertex.js';
-// import {Cube} from './cube.js';
-
-// var A = new Vertex(10, 20, 0.5);
-
-// var cube = new Cube(new Vertex(0, 0, 0), 200);
-
 var Vertex = function(x, y, z) {
     this.x = parseFloat(x);
     this.y = parseFloat(y);
